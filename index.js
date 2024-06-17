1ed90d6b031781b9d1d487ece8234be2d26b3738
Abhi Abrol <abhi.abrol34@gmail.com>
2024-06-17 17:31:19 +1000

    finishe Google Auth Flow

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there'});
+app.use(
+  session({
+    secret: keys.cookieKey,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    store: MongoStore.create({ mongoUrl: keys.mongoURI })
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 3001; 
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
