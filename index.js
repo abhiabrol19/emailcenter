c6467226c0eb8152846474bc04d924a132038d57
Abhi Abrol <abhi.abrol34@gmail.com>
2024-07-01 23:48:32 +1000

    fix index.html build file location

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ const paymentRoutes = require('./routes/paymentRoutes');
 app.use('/api', paymentRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'));
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
